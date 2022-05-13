947fab0b8dac07627e44247cbfd9cdf457a6f03a
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2022-05-13 00:04:12 -0400

    Delete human_detection directory

--- human_detection/src/lidar_sub_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <human_detection/lidar_sub.hpp>
-
-int main(int argc, char** argv){
-    ros::init(argc, argv, "lidar_sub_node");
-    ros::NodeHandle nh;
-    Laser pub(nh);
-    ros::spin();
-    return 0;
-}
