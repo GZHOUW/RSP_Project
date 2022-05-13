2a897b8adb0912dad4cf298b4a6a494226ff9e62
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2022-05-13 00:29:28 -0400

    Delete human_detection directory

--- human_detection/src/detector_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <human_detection/detector.hpp>
-
-int main(int argc, char** argv){
-    ros::init(argc, argv, "detector_node");
-    ros::NodeHandle nh;
-    Detector pub(nh);
-    ros::spin();
-    return 0;
-}
